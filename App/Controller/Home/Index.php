7202a1d99e91aaec6e405312e9cf546162e3bbbb
blacksun <18982821376@sina.cn>
2016-07-13 14:56:22 +0800

    项目测试

---
+++ App/Controller/Home/Index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/13 0013
+ * Time: 12:49
+ */
+namespace App\Controller\Home;
+class Index{
+    static function test(){
+        echo  __METHOD__;
+    }
+}
